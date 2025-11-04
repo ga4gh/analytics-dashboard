e8879035ebdcbc56a70a6a7ccbf0a2a488e038f0
dashrath chauhan <dashrath.chauhan@ga4gh.org>
2025-11-04 12:12:58 +0000

    changed config

--- src/config/config.py
+++ src/config/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     def __init__(self) -> None:
         self.database_url = os.getenv("DATABASE_URL",
-                                      "postgresql://analytics_staging:##R)n\u0026oN[a2+=}\u003eNWCNK@analytics-staging.c0q4keadjybw.us-east-2.rds.amazonaws.com:5432/analytics")
+                                      "postgresql://pgadmin:admin@localhost:5432/analytics")
 
         self.cats_api_key = os.getenv("CATS_API_KEY", "")
         self.host = os.getenv("HOST", "127.0.0.1")
